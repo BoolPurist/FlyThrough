3b0f85b5c064090f3fc9aa2b4c574f407ba9492a
BoolPurist <fnaumann@posteo.de>
2021-11-30 06:01:00 +0100

    Renderer settings in regards for rotation of skybox is now reset after playing game.

--- Assets/Code/Components/RotateSkyBox.cs
+++ Assets/Code/Components/RotateSkyBox.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class RotateSkyBox : MonoBehaviour
 {
+  private const string PROPERTY_NAME_SKYBOX = "_Rotation";
+
   [SerializeField]
   [Tooltip("Speed at which the sky box is rotating")]
   [Min(0)]
   private float _rotationSpeed = 1f;
 
+  private void Start() => RenderSettings.skybox.SetFloat(PROPERTY_NAME_SKYBOX, 0f);
+
+  private void OnDestroy() => RenderSettings.skybox.SetFloat(PROPERTY_NAME_SKYBOX, 0f);
+
+
   void FixedUpdate()
   {
-    const string PROPERTY_NAME = "_Rotation";
-    float oldRotation = RenderSettings.skybox.GetFloat(PROPERTY_NAME);
+    
+    float oldRotation = RenderSettings.skybox.GetFloat(PROPERTY_NAME_SKYBOX);
     float newRotation = (oldRotation + (_rotationSpeed * Time.deltaTime)) % 360f;
-    RenderSettings.skybox.SetFloat(PROPERTY_NAME, newRotation);
+    RenderSettings.skybox.SetFloat(PROPERTY_NAME_SKYBOX, newRotation);
   }
 }
